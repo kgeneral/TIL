79d327d6cc36f14f53885709ce0e8a7da4288b83
kgeneral <dykim1983@gmail.com>
2020-03-12 09:22:57 +0900

    Update Module.py

--- Python/Module.py
+++ Python/Module.py
@@ -3,7 +3,7 @@
 ## Make custom module and import
 - package name = name of parent directory
 - use absolute import
-```python
+`python
 from package1.package2.filename import classname 
 from package1.package2.filename import funcname
-```
+`
