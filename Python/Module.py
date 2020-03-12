3ac0731620e4ea4b9a2db0b87499a885e4a182d0
kgeneral <dykim1983@gmail.com>
2020-03-12 09:22:33 +0900

    Create Module.py

---
+++ Python/Module.py
@@ -0,0 +1,9 @@
+# Module
+
+## Make custom module and import
+- package name = name of parent directory
+- use absolute import
+```python
+from package1.package2.filename import classname 
+from package1.package2.filename import funcname
+```
